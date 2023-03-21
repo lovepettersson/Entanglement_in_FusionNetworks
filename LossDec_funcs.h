994293717799c93c979310113da1b9294e541ad8
Stefano Paesani <stefano.paesani@nbi.ku.dk>
2023-03-21 10:23:57 +0100

    Add files via upload
    Added C++ files for Gaussian Elimination

---
+++ LossDec_funcs.h
@@ -0,0 +1,15 @@
+# define data_type bool
+
+extern "C" {
+    void LossDecoder_GaussElimin(data_type*, int, int, int);
+    void LossDecoder_GaussElimin_print(data_type*, int, int, int);
+    void SwitchRows(data_type*, int, int, int, int);
+    void SubtractRows(data_type*, int, int, int, int);
+
+    void LossDecoder_GaussElimin_trackqbts(data_type*, int*, int, int, int);
+    void SwitchRows_trackqbts(data_type*, int*, int, int, int, int);
+    void SubtractRows_trackqbts(data_type*, int*, int, int, int, int);
+
+    void PrintMatrix_toTerminal(data_type*, int, int);
+    void PrintMatrix_int_toTerminal(int*, int, int);
+}
