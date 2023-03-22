614691e3c1a51a996ca16766eb87798a9491f34c
MCLoebl <matthias.loebl@rwth-aachen.de>
2023-03-22 08:14:55 +0100

    first tests for bit-field

--- LossDec_funcs.cpp
+++ LossDec_funcs.cpp
@@ -5,9 +5,10 @@
 # include <math.h>
 # include <random>
 # include <functional>
+# include <cstdlib>  // rand()
+# include <cstring>  // memcpy
 
-
-void LossDecoder_GaussElimin(data_type* mat, int n_rows, int n_cols, int num_lost_qbts) {
+void LossDecoder_GaussElimin(data_type* mat, data_type* dummy, int n_rows, int n_cols, int num_lost_qbts) {
 	int ix_r, lead, i;
 	data_type* m;
 
@@ -41,7 +42,7 @@ void LossDecoder_GaussElimin(data_type* mat, int n_rows, int n_cols, int num_los
 
 
 		if (i != ix_r) {
-			SwitchRows(mat, i, ix_r, n_rows, n_cols);
+			SwitchRowsBlock(mat, dummy, i, ix_r, n_rows, n_cols);
 		}
 
 		m = mat + lead + (ix_r + 1) * n_cols;
@@ -131,6 +132,14 @@ void SwitchRows(data_type* mat, int row0, int row1, int n_rows, int n_cols) {
 		++p0;
 		++p1;
 	}
+}
+
+void SwitchRowsBlock(data_type* mat, data_type* dummy, int row0, int row1, int n_rows, int n_cols) {
+	data_type* p0 = mat + row0 * n_cols;
+	data_type* p1 = mat + row1 * n_cols;
+	memcpy(dummy, p0, sizeof(data_type) * n_cols);
+	memcpy(p0, p1, sizeof(data_type) * n_cols);
+	memcpy(p1, dummy, sizeof(data_type) * n_cols);
 }
 
 void SubtractRows(data_type* mat, int row0, int row_target, int n_rows, int n_cols) {
@@ -360,4 +369,88 @@ void PrintMatrix_int_toTerminal(int* mat, int n_rows, int n_cols) {
 		}
 	}
 	std::cout << std::endl;
+}
+
+
+void random_maxtrix(data_type* mat, int n_rows, int n_cols){
+  for(int r=0; r<n_rows; r++){
+    for(int c=0; c<n_cols; c++){
+      mat[r*n_cols + c] = rand() % 2;
+    }
+  }
+}
+
+void print_bitwise(data_type_b* row, uint64_t num_blocks){
+  for(uint64_t i=0; i<num_blocks; i++){
+    for(uint8_t byte=0; byte<sizeof(data_type_b); byte++){
+      for(uint8_t bit=0; bit<8; bit++){
+        printf("%i", (row[i] >> (byte*8+bit)) & 1ul ? 1 : 0);
+      }
+      printf(",");
+    }
+    printf("|");
+  }
+  printf("\n");
+}
+
+int main(int argc , char** argv) {
+  int ans;
+  uint64_t cnt = 0;
+  if (argc != 2) {
+    fprintf(stderr, "Invalid number of arguments.\n");
+    return 1;
+  }
+  int nQubit = 10;
+  int n_rows = nQubit;
+  int n_cols = nQubit * 2;
+  int num_lost_qbts = nQubit;
+
+  data_type* mat = (data_type*)malloc(sizeof(data_type) * n_rows * n_cols);
+  data_type* dummy = (data_type*)malloc(sizeof(data_type) * n_cols); // dummy memory for swapping rows
+
+  random_maxtrix(mat, n_rows, n_cols);
+  PrintMatrix_toTerminal(mat, n_rows, n_cols);
+  LossDecoder_GaussElimin(mat, dummy, n_rows, n_cols, num_lost_qbts);
+  printf("%s\n", "");
+  PrintMatrix_toTerminal(mat, n_rows, n_cols);
+  printf("%s\n", "");
+
+  // some tests for bit-wise operations
+  n_cols = 100;
+  uint64_t num_blocks = n_cols / (8 * sizeof(data_type_b)) + 1;
+  uint64_t num_bytes = sizeof(data_type_b) * num_blocks;
+  data_type_b* testRow0 = (data_type_b*)malloc(num_bytes);
+  data_type_b* testRow1 = (data_type_b*)malloc(num_bytes);
+  data_type_b* testRow2 = (data_type_b*)malloc(num_bytes);
+  memset(testRow0, 250, num_bytes);
+  memset(testRow1, 7, num_bytes);
+
+  printf("bitwise or:\n");
+  for(uint64_t i=0; i<num_blocks; i++){
+    testRow2[i] = testRow0[i] | testRow1[i];
+  }
+  print_bitwise(testRow0, num_blocks);
+  print_bitwise(testRow1, num_blocks);
+  print_bitwise(testRow2, num_blocks);
+
+  printf("bitwise and:\n");
+  for(uint64_t i=0; i<num_blocks; i++){
+    testRow2[i] = testRow0[i] & testRow1[i];
+  }
+  print_bitwise(testRow0, num_blocks);
+  print_bitwise(testRow1, num_blocks);
+  print_bitwise(testRow2, num_blocks);
+
+  printf("bitwise xor:\n");
+  for(uint64_t i=0; i<num_blocks; i++){
+    testRow2[i] = testRow0[i] ^ testRow1[i];
+  }
+  print_bitwise(testRow0, num_blocks);
+  print_bitwise(testRow1, num_blocks);
+  print_bitwise(testRow2, num_blocks);
+
+  free(testRow0);free(testRow1);free(testRow2);
+
+  free(mat);
+  return 0;
 }
--- LossDec_funcs.h
+++ LossDec_funcs.h
@@ -1,9 +1,11 @@
 # define data_type bool
+# define data_type_b uint64_t
 
 extern "C" {
     void LossDecoder_GaussElimin(data_type*, int, int, int);
     void LossDecoder_GaussElimin_print(data_type*, int, int, int);
     void SwitchRows(data_type*, int, int, int, int);
+    void SwitchRowsBlock(data_type*, data_type*, int, int, int, int);
     void SubtractRows(data_type*, int, int, int, int);
 
     void LossDecoder_GaussElimin_trackqbts(data_type*, int*, int, int, int);
